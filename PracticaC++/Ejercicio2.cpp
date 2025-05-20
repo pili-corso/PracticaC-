9a31c2103d3da31ae1517a36074a6fe10f2711c2
UsuarioHI <UsuarioHI@SAC2270052>
2025-05-20 17:14:40 -0300

    cambios

---
+++ PracticaC++/Ejercicio2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // a) Usando un solo operador de inserción de flujo
+    cout << "1234" << endl;
+
+    // b) Una única sentencia con 4 operadores de inserción de flujo
+    cout << 1 << 2 << 3 << 4 << endl;
+
+    // c) Usando cuatro sentencias
+    cout << 1;
+    cout << 2;
+    cout << 3;
+    cout << 4 << endl;
+
+    return 0;
+}
